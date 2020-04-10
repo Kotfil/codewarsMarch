82112710f6d43b41f9d5d6a3d51652d314eea604
FIlipp <filippkotenko@outlook.com>
2020-04-10 21:14:46 +0300

    1

--- script.js
+++
@@ -1,9 +0,0 @@
-function doubleChar(str) {
-    // Your code here
-    var Arr = [];
-    for(var i = 0; i <= str.length-1; i++){
-        Arr.push(str[i]);
-        Arr.push(str[i]);
-    }
-    return Arr.join('');
-}
