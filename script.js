254dd20172aee88000147e52a072f72491d3d83f
FIlipp <filippkotenko@outlook.com>
2020-04-18 10:14:25 +0300

    6 kata

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-string = ('wert');
+// string = ('wert');
+//
+//
+// const removeChar = string =>
+//     string
+//         .split('')
+//         .slice(1, string.length - 1)
+//         .join('')
+//
+// console.log (removeChar);
 
+// const switchItUp = n =>
+//     [
+//         'Zero',
+//         'One',
+//         'Two',
+//         'Three',
+//         'Four',
+//         'Five',
+//         'Six',
+//         'Seven',
+//         'Eight',
+//         'Nine',
+//     ][n]
 
-const removeChar = string =>
-    string
-        .split('')
-        .slice(1, string.length - 1)
-        .join('')
-
-console.log (removeChar);
