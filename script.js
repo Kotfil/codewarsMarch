e0c4256f5eadac65a2a61210744483f05c361613
Kotfil <Dramen333>
2020-08-09 08:32:05 +0300

    redi

--- script.js
+++ script.js
@@ -14,7 +14,12 @@ let strReverse = str.split('').reverse().join('');
 
 console.log(strReverse);
 
+let arr = [1,2,3,4,5];
 
+let s = arr.reduce((acc,el) => {
+        return acc = [...acc,el]
+},0);
+console.log(s)
 // const switchItUp = n =>
 //     [
 //         'Zero',
